84490361b698722a219e6a4712934b4828f086cc
Bryan Moreno <bfourbryan@gmail.com>
2021-03-27 03:48:18 -0700

    Addition of onClick's to filter component buttons. Addition of corresponding sorting functions in directory.js.

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -3,23 +3,30 @@ import { Container } from 'react-bootstrap';
 
 
 
-function Filters({ handleInputChange }) {
+function Filters({ handleInputChange, handleSortBtnFirstNameAZ, handleSortBtnFirstNameZA, handleSortBtnLastNameAZ, handleSortBtnLastNameZA  }) {
 
     return (
 
         // Filter form inserted here to Filter thru api returned data
-        <Container style={{ marginBottom:"2rem" }}>
+        <Container style={{ marginBottom: "2rem" }}>
             <div className="card">
                 <div className="card-body">
                     <form>
                         <div className="form-group">
-                            <label htmlFor="nameFilter" style={{fontFamily:"'Montserrat', sans-serif"}}>Search by Name</label>
+                            <label htmlFor="nameFilter" style={{ fontFamily: "'Montserrat', sans-serif" }}>Search by Name</label>
                             <input type="text" className="form-control" name="search" id="nameFilter" onChange={handleInputChange} />
                         </div>
                     </form>
                 </div>
+                <div className="card-body" style={{ display: "flex", justifyContent: "center", flexWrap:"wrap" }}>
+                    <button type="button" class="btn btn-outline-info" style={{ margin:"4px" }} onClick={handleSortBtnFirstNameAZ} >First Name A-Z</button>
+                    <button type="button" class="btn btn-outline-info" style={{ margin:"4px" }} onClick={handleSortBtnFirstNameZA} >First Name Z-A</button>
+                    <button type="button" class="btn btn-outline-info" style={{ margin:"4px" }} onClick={handleSortBtnLastNameAZ} >Last Name A-Z</button>
+                    <button type="button" class="btn btn-outline-info" style={{ margin:"4px" }} onClick={handleSortBtnLastNameZA} >Last Name Z-A</button>
+                </div>
 
             </div>
+            
         </Container>
     );
 };
--- src/pages/Directory.js
+++ src/pages/Directory.js
@@ -14,36 +14,71 @@ import Image from '../components/Image';
 function Directory () {
 
     const [employees, setEmployees] = useState([]);
-    const [search, setSearch] = useState("");
-    
+    const [search, setSearch] = useState("");    
+        
     // return with componetns to build whole page...
     
     // call API when the page loads to then populate Body.js cards
     useEffect(() => {
         getEmployees();
     }, []);
-
+    
     const getEmployees = () => {
-
+        
         API.getEmployeeList().then((res) => setEmployees(res.data.results));
-            
+        
     };
-
-
     
     //handleInput... for search function
     function handleInputChange (event)  {
         const { value } = event.target;
         setSearch( value );
     };
+    
+    const [sortBy, setSortBy] = useState("");  
+    //Buttons for selecting a sort situation
+    function handleSortBtnFirstNameAZ (az) {
+        setSortBy(az);
+        let employeesSortAZ;
+        employeesSortAZ = employees.sort((a,b) => a.name.first.localeCompare(b.name.first));
+        setEmployees(employeesSortAZ);
+        console.log(sortBy, setSortBy);
+    };
 
+    function handleSortBtnFirstNameZA (za) {
+        setSortBy(za);
+        let employeesSortZA;
+        employeesSortZA = employees.sort((a,b) => b.name.first.localeCompare(a.name.first));
+        setEmployees(employeesSortZA);
+        console.log(sortBy, setSortBy);
+    };
+
+    function handleSortBtnLastNameAZ (az) {
+        setSortBy(az);
+        let employeesSortLastAZ;
+        employeesSortLastAZ = employees.sort((a,b) => a.name.last.localeCompare(b.name.last));
+        setEmployees(employeesSortLastAZ);
+        console.log(sortBy, setSortBy);
+    };
+
+    function handleSortBtnLastNameZA  (za) {
+        setSortBy(za);
+        let employeesSortLastZA;
+        employeesSortLastZA = employees.sort((a,b) => b.name.last.localeCompare(a.name.last));
+        setEmployees(employeesSortLastZA);
+        console.log(sortBy, setSortBy);
+    };
 
     return (
 
+        // Add onclick to Filters to passes button info/event 
+        //get onClick working... console log a value... then sort the array to update EMPLOYEES
+        // sort use the googles.
         <div className="directory">
             <Header />
             <Image />
-            <Filters handleInputChange={handleInputChange} />
+            <Filters handleInputChange={handleInputChange} handleSortBtnFirstNameAZ={handleSortBtnFirstNameAZ} 
+            handleSortBtnFirstNameZA={handleSortBtnFirstNameZA} handleSortBtnLastNameAZ={handleSortBtnLastNameAZ} handleSortBtnLastNameZA={handleSortBtnLastNameZA} /> 
             <Title />
             <Body employees={employees} search={search}  />
             <Footer />
